58b1512d63841a2bde6153f41d2e2d311ab3c11a
Hardik <dhardik1430@gmail.com>
2024-02-19 23:08:20 +0530

    index file added

--- index.js
+++ index.js
@@ -3,32 +3,32 @@ var app = express();
 
 const PORT = process.env.PORT || 3000;
 const fs = require("fs");
-const crypto = require('crypto');
+const crypto = require("crypto");
 app.use(express.json());
 
 app.get("/api/commits/random", function (req, res) {
-    const fileContent = fs.readFileSync(`${__dirname}/data.json`)
-    const data = JSON.parse(fileContent);
-    const random = Math.floor(Math.random() * data.length);
-    const uniqueID = crypto.randomBytes(20).toString('hex');
-    res.json({
-      ...data[random],
-      id: uniqueID
-    });
+  const fileContent = fs.readFileSync(`${__dirname}/data.json`);
+  const data = JSON.parse(fileContent);
+  const random = Math.floor(Math.random() * data.length);
+  console.log(random)
+  const uniqueID = crypto.randomBytes(16).toString("hex")+ random.toString().padStart(3, '0');
+  res.json({
+    ...data[random],
+    id: uniqueID,
   });
+});
 app.get("/api/commits", function (req, res) {
- const fileContent = fs.readFileSync(`${__dirname}/data.json`)
-    const data = JSON.parse(fileContent);   
-    res.json(data);
-
+  const fileContent = fs.readFileSync(`${__dirname}/data.json`);
+  const data = JSON.parse(fileContent);
+  res.json(data);
 });
 
 app.get("/api", function (req, res) {
-  res.send("API <a href='/api/commits'>Commits</a>")
+  res.send("API <a href='/api/commits'>Commits</a>");
 });
 
 app.get("/", function (req, res) {
-  const fileContent = fs.readFileSync(`${__dirname}/view/index.html`)
+  const fileContent = fs.readFileSync(`${__dirname}/view/index.html`);
   res.send(fileContent.toString());
 });
 app.listen(PORT, function () {
