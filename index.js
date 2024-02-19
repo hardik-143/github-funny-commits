63f5bd00039e99683dfc799ce6fbc3391512216f
Hardik <dhardik1430@gmail.com>
2024-02-19 22:50:28 +0530

    index file added

--- index.js
+++ index.js
@@ -3,13 +3,18 @@ var app = express();
 
 const PORT = process.env.PORT || 3000;
 const fs = require("fs");
+const crypto = require('crypto');
 app.use(express.json());
 
 app.get("/api/commits/random", function (req, res) {
     const fileContent = fs.readFileSync(`${__dirname}/data.json`)
     const data = JSON.parse(fileContent);
     const random = Math.floor(Math.random() * data.length);
-    res.json(data[random]);
+    const uniqueID = crypto.randomBytes(20).toString('hex');
+    res.json({
+      ...data[random],
+      id: uniqueID
+    });
   });
 app.get("/api/commits", function (req, res) {
  const fileContent = fs.readFileSync(`${__dirname}/data.json`)
@@ -23,7 +28,8 @@ app.get("/api", function (req, res) {
 });
 
 app.get("/", function (req, res) {
-  res.send("Hello World! <a href='/api'>API</a>");
+  const fileContent = fs.readFileSync(`${__dirname}/view/index.html`)
+  res.send(fileContent.toString());
 });
 app.listen(PORT, function () {
   console.log(`app listening on port ${PORT}!`);
